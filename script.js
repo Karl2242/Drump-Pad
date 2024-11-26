9cf875c0ae17b096e3cd0a66e4bef3ebac2bc6a4
Karl2242 <kchikhicloud@gmail.com>
2024-11-26 14:11:50 +0100

    record update

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const mesPad = document.querySelectorAll("div.pad > div");
 const audio = document.querySelectorAll("audio");
 let count = 0;
-let array = []
+var register = [];
+let recording = false;
+let playing = false;
 document.addEventListener("keyup", handlePressKeyPlaySound);
+document.addEventListener(`keydown`, handlePressKeyPlayAnimation);
+document.addEventListener(`keyup`, handlePressKeyPlayAnimationUp);
 
 function handlePressKeyPlaySound(event) {
   let chekCode = event.keyCode;
 
-  if (chekCode == 82) {
+  audio.forEach((mesaudio) => {
+    if (mesaudio.dataset.key == chekCode) {
+      mesaudio.play();
+    }
+  });
 
-  }
 
-audio.forEach((mesaudio) => {
-if(mesaudio.dataset.key == chekCode){
-    mesaudio.play()
+  console.log(register);
+  
+  
+  if (recording === true) {
+
+    if(chekCode != 82){
+      register.push(chekCode);
+
+  }}
 }
-
-})
-document.addEventListener(`keyup`, handlePressKeyPlayAnimation)
-
 
 function handlePressKeyPlayAnimation(event) {
   let chekCode = event.keyCode;
@@ -26,14 +35,52 @@ function handlePressKeyPlayAnimation(event) {
   mesPad.forEach((pad) => {
     if (pad.dataset.key == chekCode) {
       pad.classList.add("playing");
-      pad.classList.add("sound");
-      setTimeout(() => {
-        pad.classList.remove("playing");
-      }, 100);
-      setTimeout(() => {
-        pad.classList.remove("sound");
-      }, 100);
     }
   });
 }
+
+function handlePressKeyPlayAnimationUp(event) {
+  let chekCode = event.keyCode;
+console.log(count);
+
+
+  if (chekCode == 82 && count == 0) {
+    register = []
+    recording = true;
+    count++;
+  } else if (chekCode == 82 && count == 1) {
+    recording = false;
+    count--;
+  }
+
+  if (recording == true) {
+    monRecord(event);
+  } else {
+    monRecordAnulle(event);
+  }
+
+
+  mesPad.forEach((pad) => {
+    if (pad.dataset.key == chekCode) {
+      pad.classList.remove("playing");
+    }
+  });
+}
+
+//Animaiton Pour record
+function monRecord(event) {
+  mesPad.forEach((pad) => {
+    if (pad.dataset.key == 82 && event.keyCode == 82) {
+      pad.classList.add("playing-record");
+    }
+  });
+}
+
+//Animation annuler animation
+function monRecordAnulle(event) {
+  mesPad.forEach((pad) => {
+    if (pad.dataset.key == 82 && event.keyCode == 82) {
+      pad.classList.remove("playing-record");
+    }
+  });
 }
